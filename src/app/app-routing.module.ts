1172bfac8d7a369d0c7927ba5f812178365bb761
macsoftechnologies <macsoftechnologies@gmail.com>
2022-09-05 15:57:26 +0530

    login component creation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './Auth/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path : '',
+    component: LoginComponent
+    
+  },
+  {
+    path :'admin',
+    loadChildren: () => import('./admin/admin.module').then (m => m.AdminModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
